c346263f8d9464cc7b1af9ecdee4b1b0cc683c7c
mac <mac@macs-MacBook-Pro-2.local>
2024-01-29 15:10:48 +0100

    updated inspections

--- controllers/inspectionController.js
+++ controllers/inspectionController.js
@@ -1,13 +1,29 @@
 const Inspections = require('../models/inspection')
 
-//create inspection
+//create inspection logged in user
 const createInspection = async (req,res) => {
-    res.send("create inspection");
+  try {
+    const inspection = await Inspections.create({ ...req.body})
+    res.status(201).json({ success: true, inspection})
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error)
+    
+  }
 };
 
-//get all inspection
+//get all inspection private logged in and admin
 const getAllInspections = async (req,res) => {
-    res.send("get all inspection");
+    try {
+        const inspections = await Inspections.find().sort("-createdAt");
+        res.status(200).json({ success: true, inspections })
+      
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({err: 'internal server error'})
+        
+    }
 }
 
 module.exports ={createInspection, getAllInspections}
+
